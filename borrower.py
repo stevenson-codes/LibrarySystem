079ec3eb4a9fe58db4aaf2ba850d83db364adfd8
Stevenson Yan <stevenson.yan@sap.com>
2025-08-04 16:13:00 -0700

    Initial commit

---
+++ borrower.py
@@ -0,0 +1,4 @@
+class Borrower:
+    def __init__(self, name):
+        self.name = name
+        self.borrowed_books = []
---
+++ library.py
@@ -0,0 +1,23 @@
+class Library:
+    def __init__(self):
+        self.inventory = []
+    
+    def add_books(self, book):
+        self.inventory.append(book)
+    
+    def checkout_book(self, book, borrower):
+        if book in self.inventory and book.available:
+            book.availble = False
+            borrower.borrowed_books.append(book)
+        else:
+            print(book.title + " is not available.")
+
+    def return_book(self, book, borrower):
+        if book in borrower.borrowed_books:
+            book.available = True
+            borrower.borrowed_books.remove(book)
+        else:
+            print(borrower.name + " didn't borrow " + book.title + ".")
+
+    def list_inventory(self):
+        return [book.title for book in self.inventory if book.available]
